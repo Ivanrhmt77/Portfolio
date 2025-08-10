65aacddbf468a31355576af82eb4cca9f088e5e7
Ivanrhmt77 <ivanrahmat.p77@gmail.com>
2025-08-11 03:52:46 +0700

    feat: add tech stack

---
+++ src/data/tech-stack.ts
@@ -0,0 +1,58 @@
+export const techStack = [
+  {
+    name: "Flutter",
+    logo: "/icons/tech-stack/flutter.svg",
+  },
+  {
+    name: "Dart",
+    logo: "/icons/tech-stack/dart.svg",
+  },
+  {
+    name: "SQLite",
+    logo: "/icons/tech-stack/sqlite.svg",
+  },
+  {
+    name: "React",
+    logo: "/icons/tech-stack/react.svg",
+  },
+  {
+    name: "Tailwind",
+    logo: "/icons/tech-stack/tailwind.svg",
+  },
+  {
+    name: "HTML",
+    logo: "/icons/tech-stack/html.svg",
+  },
+  {
+    name: "CSS",
+    logo: "/icons/tech-stack/css.svg",
+  },
+  {
+    name: "JavaScript",
+    logo: "/icons/tech-stack/javascript.svg",
+  },
+  {
+    name: "Laravel",
+    logo: "/icons/tech-stack/laravel.svg",
+  },
+  {
+    name: "PHP",
+    logo: "/icons/tech-stack/php.svg",
+  },
+  {
+    name: "PostgreSQL",
+    logo: "/icons/tech-stack/postgresql.svg",
+  },
+  {
+    name: "Figma",
+    logo: "/icons/tech-stack/figma.svg",
+  },
+  {
+    name: "Git",
+    logo: "/icons/tech-stack/git.svg",
+  },
+  {
+    name: "CachyOS",
+    logo: "/icons/tech-stack/cachyos.svg",
+  },
+];
